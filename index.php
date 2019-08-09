c12a144a799b4cc7c5c17ef1ff9685744578df55
Анна <shesterikofff@mail.ru>
2019-08-09 10:25:46 +0300

    Поправила стиль кода в блоке php

--- index.php
+++ index.php
@@ -29,20 +29,20 @@ $user_name = 'Анна';
         <nav class="user-menu">
 
         <?php if ($is_auth === 1): ?>
-        <div class="user-menu__logged">
-            <p>$is_auth</p>
-            <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-            <a class="user-menu__logout" href="#">Выход</a>
-        </div>
+            <div class="user-menu__logged">
+                <p>$is_auth</p>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__logout" href="#">Выход</a>
+            </div>
         <?php else: ?>
-        <ul class="user-menu__list">
-            <li class="user-menu__item">
-                <a href="#">Регистрация</a>
-            </li>
-            <li class="user-menu__item">
-                <a href="#">Вход</a>
-            </li>
-        </ul>
+            <ul class="user-menu__list">
+                <li class="user-menu__item">
+                    <a href="#">Регистрация</a>
+                </li>
+                <li class="user-menu__item">
+                    <a href="#">Вход</a>
+                </li>
+            </ul>
         <?php endif ?>
 
         </nav>
