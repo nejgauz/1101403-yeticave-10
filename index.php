91d5cc0c861ba3efbef1b63af6b5fd6fc2931e98
Анна <shesterikofff@mail.ru>
2019-08-12 23:44:32 +0300

    Исправила тег на шорт-тег

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ $cards = [
 
         <?php if ($is_auth === 1): ?>
             <div class="user-menu__logged">
-                <p><?php print($user_name); ?></p>
+                <p><?=$user_name;?></p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
