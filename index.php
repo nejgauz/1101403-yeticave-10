92be6c14d9388d2cbb778c22245e2944176f3071
Анна <shesterikofff@mail.ru>
2019-08-15 14:33:01 +0300

    Убрала дублирование функции в библиотеках

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ $userName = 'Анна';
 
 require_once('functions.php');
 require_once('data.php');
+require_once('helpers.php');
 
-$pageContent = includeTemplate('main.php', ['cards' => $cards, 'categories' => $categories]);
+$pageContent = include_template('main.php', ['cards' => $cards, 'categories' => $categories]);
 
-$layoutContent = includeTemplate('layout.php', [
+$layoutContent = include_template('layout.php', [
     'content' => $pageContent,
     'categories' => $categories,
     'isAuth' => $isAuth,
