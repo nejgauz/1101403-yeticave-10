a981f4f1e9bbe91c43494412715265b6ba2a9789
Анна <shesterikofff@mail.ru>
2019-08-22 23:17:06 +0300

    Изменила логику в index.php

--- index.php
+++ index.php
@@ -6,32 +6,25 @@ require_once('functions.php');
 require_once('helpers.php');
 
 $con = getConnect();
-if (is_string($con)) {
-    $pageContent = include_template('error.php', ['error' => $con]);
-    echo $pageContent;
-} else {
+if ($con) {
     $cards = getCards($con);
     $categories = getCategories($con);
-    if (is_array($cards)  && is_array($categories)) {
-        $pageContent = include_template('main.php', ['cards' => $cards, 'categories' => $categories]);
-        $layoutContent = include_template('layout.php', [
-            'content' => $pageContent,
-            'categories' => $categories,
-            'isAuth' => $isAuth,
-            'userName' => $userName,
-            'title' => 'YetiCave - Главная страница'
-        ]);
-        echo $layoutContent;
-    } else {
-        if (is_string($cards)) {
-            $pageContent = include_template('error.php', ['error' => $cards]);
-            echo $pageContent;
-        } elseif (is_string($categories)) {
-            $pageContent = include_template('error.php', ['error' => $categories]);
-            echo $pageContent;
-        }
-    }
+    $pageContent = include_template('main.php', ['cards' => $cards, 'categories' => $categories]);
+    $layoutContent = include_template('layout.php', [
+        'content' => $pageContent,
+        'categories' => $categories,
+        'isAuth' => $isAuth,
+        'userName' => $userName,
+        'title' => 'YetiCave - Главная страница'
+    ]);
+    echo $layoutContent;
+} else {
+    $error = errorFilter('connect', $con);
+    $pageContent = include_template('error.php', ['error' => $error]);
+    echo $pageContent;
 }
+
+
 
 
 
