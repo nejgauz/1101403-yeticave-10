574430beeac2af54f0d197a357e78a9090a804f0
Анна <shesterikofff@mail.ru>
2019-08-22 21:32:52 +0300

    Убрала запросы в функции

--- functions.php
+++ functions.php
@@ -35,40 +35,81 @@ function timeCounter(string $endDate): array
 }
 
 /**
- * Обрабатывает ошибки подключения к БД и показывает страницу с ее типом
+ * Обрабатывает ошибки подключения к БД и возвращает страницу с ее типом
  * @param string $mistake вид ошибки: 'connect' (ошибка подключения) или 'request' (ошибка запроса)
  * @param $con ресурс соединения
+ * @return шаблон страницы с ошибкой
  */
-function errorFilter(string $mistake, $con)
+function errorFilter(string $mistake, $con = null)
 {
     if ($mistake === 'connect') {
         $error = mysqli_connect_error();
-        $pageContent = include_template('error.php', ['error' => $error]);
-        echo $pageContent;
     } else if ($mistake === 'request') {
         $error = mysqli_error($con);
-        $pageContent = include_template('error.php', ['error' => $error]);
-        echo $pageContent;
     }
+    return $error;
 }
 
 /**
  * Обрабатывает запросы на чтение из БД
  * @param string $sql текст запроса к БД
  * @param $con ресурс соединения
- * @return array двумерный ассоциативный массив с результатами запроса к БД
+ * @return array двумерный ассоциативный массив с результатами запроса к БД или текст ошибки
  */
-function readFromDatabase(string $sql, $con): array
+function readFromDatabase(string $sql, $con)
 {
-    if (!$con) {
-        errorFilter('connect', NULL);
+    $data = mysqli_query($con, $sql);
+    if (!$data) {
+        return errorFilter('request', $con);
     } else {
-        $data = mysqli_query($con, $sql);
-        if (!$data) {
-            errorFilter('request', $con);
-        } else {
-            $data = mysqli_fetch_all($data, MYSQLI_ASSOC);
-            return $data;
-        }
+        $data = mysqli_fetch_all($data, MYSQLI_ASSOC);
+        return $data;
     }
+}
+
+/**
+ * Возвращает список категорий по запросу
+ * @param $connection ресурс соединения
+ * @return array возвращает двумерный массив категорий или текст ошибки
+ */
+function getCategories($connection)
+{
+    $request = "SELECT `name`, symb_code FROM categories";
+    $categories = readFromDatabase($request, $connection);
+
+    return $categories;
+}
+
+/**
+ * Возвращает список лотов по запросу
+ * @param $connection ресурс соединения
+ * @return array возвращает двумерный массив лотов или текст ошибки
+ */
+function getCards($connection)
+{
+    $request = "SELECT title AS name, st_price AS price, image_path AS url, c.name AS category, dt_end AS `time`
+    FROM lots AS l
+    LEFT JOIN categories AS c
+    ON c.id = l.cat_id
+    WHERE win_id IS NULL AND dt_end > NOW()
+    ORDER BY l.dt_create DESC LIMIT 9";
+    $cards = readFromDatabase($request, $connection);
+
+    return $cards;
+}
+
+/**
+ * Подключается к базе и задает кодировку
+ * @return возвращает ресурс соединения или текст ошибки
+ */
+function getConnect()
+{
+    $connection = mysqli_connect('127.0.0.1', 'root', '', 'yeticave');
+    if (!$connection) {
+        $result = errorFilter('connect', null);
+    } else {
+        mysqli_set_charset($connection, 'utf8');
+        $result = $connection;
+    }
+    return $result;
 }
