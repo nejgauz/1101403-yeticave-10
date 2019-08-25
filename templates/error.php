6d6f7802d3bce0fd11b7a8b8364a4911ab68d3b7
Анна <shesterikofff@mail.ru>
2019-08-25 12:09:00 +0300

    Удалила ненужный файл

--- templates/error.php
+++ templates/error.php
@@ -2,7 +2,7 @@
     <div class="promo">
         <h2 class="promo__title">Ошибка:</h2>
         <article>
-            <p class="promo__text"><?=$error;?></p>
+            <p class="promo__text"><?=$error['error'];?></p>
         </article>
     </div>
 </main>
