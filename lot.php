fa8d7e633e906cad49ebad2f7ded5d829657edb3
Анна <shesterikofff@mail.ru>
2019-08-27 16:00:05 +0300

    Добавила сценарий показа лота и шаблон

---
+++ lot.php
@@ -0,0 +1,29 @@
+<?php
+$isAuth = rand(0, 1);
+$userName = 'Анна';
+
+require_once('init.php');
+
+if ($con) {
+    if (isset($_GET['lot_id'])) {
+        $card = getCard($con, $_GET['lot_id']);
+        $categories = getCategories($con);
+        $pageContent = include_template('lot_card.php', ['card' => $card, 'categories' => $categories, 'connection' => $con]);
+        $layoutContent = include_template('layout.php', [
+            'content' => $pageContent,
+            'categories' => $categories,
+            'isAuth' => $isAuth,
+            'userName' => $userName,
+            'title' => $card[0]['name'],
+            'class' => null
+        ]);
+        echo $layoutContent;
+    } else {
+        http_response_code(404);
+    }
+} else {
+    $error = errorFilter('connect', $con);
+    $pageContent = include_template('error.php', ['error' => $error]);
+    echo $pageContent;
+}
+
---
+++ templates/lot_card.php
@@ -0,0 +1,53 @@
+<nav class="nav">
+    <ul class="nav__list container">
+        <?php foreach($categories as $value): ?>
+            <li class="nav__item">
+                <a href="pages/all-lots.html"><?=strip_tags($value['name']);?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
+<section class="lot-item container">
+    <h2><?=$card[0]['name'];?></h2>
+    <div class="lot-item__content">
+        <div class="lot-item__left">
+            <div class="lot-item__image">
+                <img src="../<?=$card[0]['url'];?>" width="730" height="548" alt="Сноуборд">
+            </div>
+            <p class="lot-item__category">Категория: <span><?=$card[0]['category'];?></span></p>
+            <p class="lot-item__description"><?=$card[0]['description'];?></p>
+        </div>
+        <div class="lot-item__right">
+            <div class="lot-item__state">
+                <?php $timeClass = 'lot-item__timer timer';
+                $time = timeCounter($card[0]['time']);
+                $timeStr = $time['hours'] . ':' . $time['mins'];
+                if ($time['hours'] < 1 && $time['days'] == 0) {
+                    $timeClass .= ' timer--finishing';
+                } ?>
+                <div class="<?=$timeClass;?>">
+                    <?=$timeStr;?>
+                </div>
+                <div class="lot-item__cost-state">
+                    <div class="lot-item__rate">
+                        <span class="lot-item__amount">Текущая цена</span>
+                        <span class="lot-item__cost">
+                            <?php
+                            $maxBid = getMaxBid($connection , $card[0]['id']);
+                            $curPrice = $card[0]['st_price'];
+                            if ($curPrice > $maxBid) {
+                                echo priceFormat($curPrice);
+                            } else {
+                                echo priceFormat($maxBid['max_price']);
+                            }
+                            ?>
+                        </span>
+                    </div>
+                    <div class="lot-item__min-cost">
+                        Мин. ставка <span><?=$card[0]['step'];?> р</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
