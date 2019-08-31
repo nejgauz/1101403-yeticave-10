5873edd2a557ca6bcfb9df52610e7f342c04e742
Анна <shesterikofff@mail.ru>
2019-08-31 21:49:57 +0300

    Добавила валидацию в форму добавления лота

--- add.php
+++ add.php
@@ -1,37 +1,84 @@
 <?php
-$isAuth = rand(0, 1);
-$userName = 'Анна';
-
 require_once('init.php');
 
 if ($con) {
-    $categories = getCategories($con);
-    $pageContent = include_template('add_lot.php', ['categories' => $categories, 'connection' => $con]);
-    $layoutContent = include_template('layout.php', [
-        'content' => $pageContent,
-        'categories' => $categories,
-        'isAuth' => $isAuth,
-        'userName' => $userName,
-        'title' => 'Добавление лота',
-        'class' => null,
-        'isMain' => false,
-        'isAdd' => true
-    ]);
-    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        $categories = getCategories($con);
+        $pageContent = include_template('add_lot.php', ['categories' => $categories, 'connection' => $con, 'errors' => $errors]);
+        $layoutContent = include_template('layout.php', [
+            'content' => $pageContent,
+            'categories' => $categories,
+            'isAuth' => $isAuth,
+            'userName' => $userName,
+            'title' => 'Добавление лота',
+            'class' => null,
+            'isMain' => false,
+            'isAdd' => true
+        ]);
         echo $layoutContent;
     } else {
         $lot = $_POST;
-
-        $filename = 'uploads/' . uniqid() . '.jpg';
-        $lot['path'] = $filename;
-        move_uploaded_file($_FILES['image']['tmp_name'], $filename);
-        if (insertLotInDb($con, $lot)) {
-            $lot_id = mysqli_insert_id($con);
-            header("Location: lot.php?lot_id=" . $lot_id);
+        $requiredFields = $lot;
+        foreach ($requiredFields as $field => $value) {
+            if (isFieldEmpty($field)) {
+                $errors[$field] = isFieldEmpty($field);
+                unset($requiredFields[$field]);
+            }
+        }
+        if (!empty($_FILES['image']['tmp_name'])) {
+            $extension = pathinfo($_FILES['image']['tmp_name'], PATHINFO_EXTENSION);
+            $filename = 'uploads/' . uniqid() . $extension;
+            move_uploaded_file($_FILES['image']['tmp_name'], $filename);
+            if (validateImg($filename)) {
+                $errors['image'] = validateImg($filename);
+                unlink($filename);
+            } else {
+                $lot['path'] = $filename;
+            }
         } else {
-            $error = errorFilter('request', $con);
-            $pageContent = include_template('error.php', ['error' => $error]);
-            echo $pageContent;
+            $errors['image'] = 'Изображение не выбрано';
+        }
+        $rules = [
+            'lot-rate' => function () {
+                return validatePrice('lot-rate');
+            },
+            'lot-date' => function () {
+                return validateData('lot-date');
+            },
+            'lot-step' => function () {
+                return validateStep('lot-step');
+            }
+        ];
+        foreach ($requiredFields as $field => $value) {
+            if (isset($rules[$field])) {
+                $rule = $rules[$field];
+                $errors[$field] = $rule();
+            }
+        }
+        $errors = array_filter($errors);
+        if (empty($errors)) {
+            if (insertLotInDb($con, $lot)) {
+                $lot_id = mysqli_insert_id($con);
+                header("Location: lot.php?lot_id=" . $lot_id);
+            } else {
+                $error = errorFilter('request', $con);
+                $pageContent = include_template('error.php', ['error' => $error]);
+                echo $pageContent;
+            }
+        } else {
+            $categories = getCategories($con);
+            $pageContent = include_template('add_lot.php', ['categories' => $categories, 'connection' => $con, 'errors' => $errors]);
+            $layoutContent = include_template('layout.php', [
+                'content' => $pageContent,
+                'categories' => $categories,
+                'isAuth' => $isAuth,
+                'userName' => $userName,
+                'title' => 'Добавление лота',
+                'class' => null,
+                'isMain' => false,
+                'isAdd' => true
+            ]);
+            echo $layoutContent;
         }
     }
 } else {
--- templates/add_lot.php
+++ templates/add_lot.php
@@ -7,31 +7,31 @@
         <?php endforeach; ?>
     </ul>
 </nav>
-<form class="form form--add-lot container form--invalid" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
+<form class="form form--add-lot container <?php if (!empty($errors)): echo 'form--invalid'; endif; ?>>" action="add.php" method="post" enctype="multipart/form-data">
     <h2>Добавление лота</h2>
     <div class="form__container-two">
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+        <div class="form__item <?=errorClass($errors, 'lot-name');?>">
             <label for="lot-name">Наименование <sup>*</sup></label>
-            <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота">
-            <span class="form__error">Введите наименование лота</span>
+            <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" value="<?=getPostVal('lot-name');?>">
+            <span class="form__error"><?=$errors['lot-name'] ?? '';?></span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?=errorClass($errors, 'category');?>">
             <label for="category">Категория <sup>*</sup></label>
             <select id="category" name="category">
                 <option>Выберите категорию</option>
                 <?php foreach ($categories as $value): ?>
-                    <option value="<?=$value['id'];?>"><?=strip_tags($value['name']);?></option>
+                    <option value="<?=$value['id'];?>" <?php if ($value['name'] == getPostVal('category')): echo 'selected'; endif; ?>><?=strip_tags($value['name']);?></option>
                 <?php endforeach; ?>
             </select>
-            <span class="form__error">Выберите категорию</span>
+            <span class="form__error"><?=$errors['category'] ?? '';?></span>
         </div>
     </div>
-    <div class="form__item form__item--wide">
+    <div class="form__item form__item--wide <?=errorClass($errors, 'message');?>">
         <label for="message">Описание <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите описание лота"></textarea>
-        <span class="form__error">Напишите описание лота</span>
+        <textarea id="message" name="message" placeholder="Напишите описание лота"><?=getPostVal('message');?></textarea>
+        <span class="form__error"><?=$errors['message'] ?? '';?></span>
     </div>
-    <div class="form__item form__item--file">
+    <div class="form__item form__item--file" <?=errorClass($errors, 'image');?>>
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
             <input class="visually-hidden" type="file" name="image" id="lot-img" value="">
@@ -41,20 +41,20 @@
         </div>
     </div>
     <div class="form__container-three">
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?=errorClass($errors, 'lot-rate');?>">
             <label for="lot-rate">Начальная цена <sup>*</sup></label>
-            <input id="lot-rate" type="text" name="lot-rate" placeholder="0">
-            <span class="form__error">Введите начальную цену</span>
+            <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?=getPostVal('lot-rate');?>">
+            <span class="form__error"><?=$errors['lot-rate'] ?? '';?></span>
         </div>
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?=errorClass($errors, 'lot-step');?>">
             <label for="lot-step">Шаг ставки <sup>*</sup></label>
-            <input id="lot-step" type="text" name="lot-step" placeholder="0">
-            <span class="form__error">Введите шаг ставки</span>
+            <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?=getPostVal('lot-step');?>">
+            <span class="form__error"><?=$errors['lot-step'] ?? '';?></span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?=errorClass($errors, 'lot-date');?>">
             <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
-            <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
-            <span class="form__error">Введите дату завершения торгов</span>
+            <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?=getPostVal('lot-date');?>">
+            <span class="form__error"><?=$errors['lot-date'] ?? '';?></span>
         </div>
     </div>
     <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
