4331675212f02f80b65c263e6d6cfe5bcd85e18f
Анна <shesterikofff@mail.ru>
2019-09-02 23:05:13 +0300

    Убрала лишние уровни вложенности в сценарии add.php

--- add.php
+++ add.php
@@ -1,8 +1,48 @@
 <?php
 require_once('init.php');
 
-if ($con) {
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+if (!$con) {
+    $error = errorFilter('connect', $con);
+    $pageContent = include_template('error.php', ['error' => $error]);
+    echo $pageContent;
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $categories = getCategories($con);
+    $pageContent = include_template('add_lot.php', ['categories' => $categories, 'connection' => $con, 'errors' => $errors]);
+    $layoutContent = include_template('layout.php', [
+        'content' => $pageContent,
+        'categories' => $categories,
+        'isAuth' => $isAuth,
+        'userName' => $userName,
+        'title' => 'Добавление лота',
+        'class' => null,
+        'isMain' => false,
+        'isAdd' => true
+    ]);
+    echo $layoutContent;
+} else {
+    $lot = $_POST;
+    $errors = validateLotForm($lot);
+    if (!validateImg($_FILES['image']['tmp_name'])) {
+        $extension = pathinfo($_FILES['image']['tmp_name'], PATHINFO_EXTENSION);
+        $filename = 'uploads/' . uniqid() . $extension;
+        move_uploaded_file($_FILES['image']['tmp_name'], $filename);
+        $lot['path'] = $filename;
+    } else {
+        $errors['image'] = validateImg($_FILES['image']['tmp_name']);
+    }
+    if (empty($errors)) {
+        if (insertLotInDb($con, $lot)) {
+            $lot_id = mysqli_insert_id($con);
+            header("Location: lot.php?lot_id=" . $lot_id);
+        } else {
+            $error = errorFilter('request', $con);
+            $pageContent = include_template('error.php', ['error' => $error]);
+            echo $pageContent;
+        }
+    } else {
         $categories = getCategories($con);
         $pageContent = include_template('add_lot.php', ['categories' => $categories, 'connection' => $con, 'errors' => $errors]);
         $layoutContent = include_template('layout.php', [
@@ -16,46 +56,7 @@ if ($con) {
             'isAdd' => true
         ]);
         echo $layoutContent;
-    } else {
-        $lot = $_POST;
-        $errors = validateLotForm($lot);
-        if (!validateImg($_FILES['image']['tmp_name'])) {
-            $extension = pathinfo($_FILES['image']['tmp_name'], PATHINFO_EXTENSION);
-            $filename = 'uploads/' . uniqid() . $extension;
-            move_uploaded_file($_FILES['image']['tmp_name'], $filename);
-            $lot['path'] = $filename;
-        } else {
-            $errors['image'] = validateImg($_FILES['image']['tmp_name']);
-        }
-        if (empty($errors)) {
-            if (insertLotInDb($con, $lot)) {
-                $lot_id = mysqli_insert_id($con);
-                header("Location: lot.php?lot_id=" . $lot_id);
-            } else {
-                $error = errorFilter('request', $con);
-                $pageContent = include_template('error.php', ['error' => $error]);
-                echo $pageContent;
-            }
-        } else {
-            $categories = getCategories($con);
-            $pageContent = include_template('add_lot.php', ['categories' => $categories, 'connection' => $con, 'errors' => $errors]);
-            $layoutContent = include_template('layout.php', [
-                'content' => $pageContent,
-                'categories' => $categories,
-                'isAuth' => $isAuth,
-                'userName' => $userName,
-                'title' => 'Добавление лота',
-                'class' => null,
-                'isMain' => false,
-                'isAdd' => true
-            ]);
-            echo $layoutContent;
-        }
     }
-} else {
-    $error = errorFilter('connect', $con);
-    $pageContent = include_template('error.php', ['error' => $error]);
-    echo $pageContent;
 }
 
 
