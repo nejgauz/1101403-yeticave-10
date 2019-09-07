d04015bad70a06f51a4599a41b6c2b164be59201
Анна <shesterikofff@mail.ru>
2019-09-08 00:35:22 +0300

    Разграничила страницу показа лота на публичную и приватную части

--- templates/lot_card.php
+++ templates/lot_card.php
@@ -52,6 +52,16 @@
                         </span>
                     </div>
                 </div>
+                <?php if (isset($_SESSION['name'])): ?>
+                    <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post" autocomplete="off">
+                        <p class="lot-item__form-item form__item form__item--invalid">
+                            <label for="cost">Ваша ставка</label>
+                            <input id="cost" type="text" name="cost" placeholder="12 000">
+                            <span class="form__error">Введите наименование лота</span>
+                        </p>
+                        <button type="submit" class="button">Сделать ставку</button>
+                    </form>
+                <?php endif; ?>
             </div>
         </div>
     </div>
