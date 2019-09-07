d1b6073229649c2b5e5891e2ca6f2e8b98aa9e54
Анна <shesterikofff@mail.ru>
2019-09-07 23:42:24 +0300

    Добавила ссылку на форму входа

--- templates/layout.php
+++ templates/layout.php
@@ -36,7 +36,7 @@
                             <a href="sign_up.php">Регистрация</a>
                         </li>
                         <li class="user-menu__item">
-                            <a href="#">Вход</a>
+                            <a href="login.php">Вход</a>
                         </li>
                     </ul>
                 <?php endif; ?>
