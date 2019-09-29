96d9f41ea9e7cb8951b40cfb37fb976e6a0f2228
Анна <shesterikofff@mail.ru>
2019-09-29 15:45:46 +0300

    Поправила схему БД

--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE lots (
     cat_id     INT NOT NULL,
     win_id     INT DEFAULT NULL,
     dt_create  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    title      CHAR(255),
+    title      CHAR(64),
     descr      TEXT(65535),
     image_path CHAR(255),
     st_price   INT,
@@ -25,15 +25,15 @@ CREATE TABLE lots (
 CREATE TABLE users (
     id         INT AUTO_INCREMENT PRIMARY KEY,
     dt_reg     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    email      CHAR(255) UNIQUE,
-    name       CHAR(255),
+    email      CHAR(64) UNIQUE,
+    name       CHAR(64),
     password   CHAR(255),
     contact    CHAR(255)
 );
 CREATE TABLE categories (
     id         INT AUTO_INCREMENT PRIMARY KEY,
-    name       CHAR(255) NOT NULL,
-    symb_code  CHAR(255) NOT NULL
+    name       CHAR(64) NOT NULL,
+    symb_code  CHAR(64) NOT NULL
 );
 CREATE TABLE stop_words (
     id         INT AUTO_INCREMENT PRIMARY KEY,
@@ -43,5 +43,4 @@ CREATE UNIQUE INDEX u_email ON users(email);
 CREATE INDEX bid ON bids(price);
 CREATE INDEX title ON lots(title);
 CREATE INDEX step ON lots(step);
-CREATE INDEX descr ON lots(descr);
 CREATE FULLTEXT INDEX lot_search ON lots(title, descr);
