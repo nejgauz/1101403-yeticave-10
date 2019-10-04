7ec3c2807eaa3f2eee4a8002b324f36eb8b552f1
Анна <shesterikofff@mail.ru>
2019-10-04 20:04:42 +0300

    ДОбавила проверку на пустоту лота при обращении к массиву в сценарии добавления лота

--- add.php
+++ add.php
@@ -25,37 +25,40 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 $lot = $_POST;
-$errors = validateLotForm($lot);
-if (!validateImg($_FILES['image']['tmp_name']) && empty($errors)) {
-    $extension = pathinfo($_FILES['image']['tmp_name'], PATHINFO_EXTENSION);
-    $filename = 'uploads/' . uniqid() . $extension;
-    move_uploaded_file($_FILES['image']['tmp_name'], $filename);
-    $lot['path'] = $filename;
-} else {
-    $errors['image'] = validateImg($_FILES['image']['tmp_name']);
+if (!empty($lot)) {
+    $errors = validateLotForm($lot);
+    if (!validateImg($_FILES['image']['tmp_name']) && empty($errors)) {
+        $extension = pathinfo($_FILES['image']['tmp_name'], PATHINFO_EXTENSION);
+        $filename = 'uploads/' . uniqid() . $extension;
+        move_uploaded_file($_FILES['image']['tmp_name'], $filename);
+        $lot['path'] = $filename;
+    } else {
+        $errors['image'] = validateImg($_FILES['image']['tmp_name']);
+    }
+
+    if (empty($errors)) {
+        $lot['user_id'] = $_SESSION['id'];
+        foreach ($lot as $key => $value) {
+            $lot[$key] = strip_tags($value);
+        }
+        if (insertLotInDb($con, $lot)) {
+            $lot_id = mysqli_insert_id($con);
+            header("Location: lot.php?lot_id=" . $lot_id);
+            exit();
+        }
+        $error = errorFilter('request', $con);
+        $pageContent = include_template('error.php', ['error' => $error]);
+        $layoutContent = include_template('layout.php', [
+            'content' => $pageContent,
+            'categories' => $categories,
+            'title' => 'Ошибка',
+        ]);
+        echo $layoutContent;
+    } else {
+        $layoutContent = addPage($categories, $con, $errors);
+        echo $layoutContent;
+    }
 }
 
-if (empty($errors)) {
-    $lot['user_id'] = $_SESSION['id'];
-    foreach ($lot as $key => $value) {
-        $lot[$key] = strip_tags($value);
-    }
-    if (insertLotInDb($con, $lot)) {
-        $lot_id = mysqli_insert_id($con);
-        header("Location: lot.php?lot_id=" . $lot_id);
-        exit();
-    }
-    $error = errorFilter('request', $con);
-    $pageContent = include_template('error.php', ['error' => $error]);
-    $layoutContent = include_template('layout.php', [
-        'content' => $pageContent,
-        'categories' => $categories,
-        'title' => 'Ошибка',
-    ]);
-    echo $layoutContent;
-} else {
-    $layoutContent = addPage($categories, $con, $errors);
-    echo $layoutContent;
-}
 
 
